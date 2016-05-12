75dc0726e2d431bb2f6a6f72e7071aa4eae99c15
Julusian <git@julusian.co.uk>
2016-05-12 10:03:55 +0000

    Remove unused define from config.h

--- config.h
+++ config.h
@@ -1,6 +1,3 @@
-
-#define CLIENT_TYPE "artnet-node"
-
 #define LOCAL_IP      10,   42,   13,   49           // the IP address of node
 #define BROADCAST_IP  10,   42,   13,  255           // broadcast IP address
 #define GATEWAY_IP    10,   42,   13,    2           // gateway IP address (use ip address of controller)
